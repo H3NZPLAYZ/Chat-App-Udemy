22278c6f1eb9a478a1af441475dfc6fc3f480eac
H3NZPLAYZ <c.hugo.g.maldonado@gmail.com>
2026-04-21 15:16:40 +0200

    Chapter 53

--- core/consumers.py
+++ core/consumers.py
@@ -2,11 +2,26 @@
 import json
 
 from channels.generic.websocket import WebsocketConsumer
+from django.core.mail import message
+
+from core.models import Room, Message
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.room = self.get_room()
+        if not self.room:
+            self.close()
+            return
+        self.user = self.scope["user"]
         self.accept()
+
+    def get_room(self):
+        self.room_slug = self.scope["url_route"]["kwargs"]["room_slug"]
+        try:
+            return Room.objects.get(slug=self.room_slug)
+        except Room.DoesNotExist:
+            return None
 
     def disconnect(self, close_code):
         pass
@@ -14,5 +29,13 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        Message.objects.create(
+            room=self.room,
+            user=self.user,
+            content=message,
+        )
+        message = text_data_json["message"]
+
+
 
         self.send(text_data=json.dumps({"message": message}))
