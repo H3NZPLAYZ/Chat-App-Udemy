4b85455da7a2a1020a3540b1c3151bfae7fc581d
H3NZPLAYZ <c.hugo.g.maldonado@gmail.com>
2026-04-21 10:09:08 +0200

    Chapter 45

--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from core.models import Room
+
+
 # Create your views here.
 def index(request):
-    context = {}
+
+    context = {
+        'rooms': Room.objects.all()
+    }
     return render(request, 'index.html', context)
