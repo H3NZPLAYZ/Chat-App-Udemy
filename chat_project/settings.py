a212eabf69d967b1c968f7ec3a5cfb75020225b7
H3NZPLAYZ <c.hugo.g.maldonado@gmail.com>
2026-04-21 16:19:13 +0200

    Chapter 56

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -127,6 +127,9 @@ ASGI_APPLICATION = "chat_project.asgi.application"
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
